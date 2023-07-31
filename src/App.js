74d14490e66c5eb9d470bc3701ff3ce9bc3ee381
tubi55 <tubi55@nate.com>
2023-07-31 15:12:07 +0900

    auto batching 테스트

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useState } from 'react';
+
+function fetchSomething() {
+	return new Promise((res) => setTimeout(res, 100));
+}
+
 function App() {
+	const [Count, setCount] = useState(0);
+	const [Count2, setCount2] = useState(0);
+
+	const handleClick = () => {
+		fetchSomething().then(() => {
+			setCount((c) => c + 1);
+			setCount2((c) => c + 2);
+		});
+		//setCount(Count + 1);
+		//setCount2(Count2 + 1);
+	};
+
+	console.log('Rendered!!');
+
 	return (
 		<div className='App'>
-			<h1>Hello</h1>
+			<h1>
+				{Count}-{Count2}
+			</h1>
+			<button onClick={handleClick}>button</button>
 		</div>
 	);
 }
