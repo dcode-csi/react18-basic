20004fb6ba4fb5540dc2d04991c75c89ee0a3b97
tubi55 <tubi55@nate.com>
2023-07-31 17:02:25 +0900

    useTransition이 없을떄

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
 import { useState } from 'react';
-import { flushSync } from 'react-dom';
-
-function fetchSomething() {
-	return new Promise((res) => setTimeout(res, 100));
-}
 
 function App() {
 	const [Count, setCount] = useState(0);
-	const [Count2, setCount2] = useState(0);
+	const [Items, setItems] = useState([]);
 
 	const handleClick = () => {
-		fetchSomething().then(() => {
-			//flushSync를 활용하면 예전처럼 State마다 개별적으로 리랜더 가능
-			flushSync(() => setCount((c) => c + 1));
-			setCount((c) => c + 1);
-			setCount2((c) => c + 2);
-		});
-	};
+		setCount(Count + 1);
+		const arr = Array(20000)
+			.fill(1)
+			.map((el, idx) => Count + 20000 - idx);
 
-	console.log('Rendered!!');
+		setItems(arr);
+	};
 
 	return (
 		<div className='App'>
-			<h1>
-				{Count}-{Count2}
-			</h1>
-			<button onClick={handleClick}>button</button>
+			<button onClick={handleClick}>{Count}</button>
+			<ul>
+				{Items.map((item) => (
+					<li key={item}>{item}</li>
+				))}
+			</ul>
 		</div>
 	);
 }
@@ -33,7 +28,6 @@ function App() {
 export default App;
 
 /*
-	Automatic Batching
-	- 특정 이벤트 핸들러 함수 안쪽에서 복수개의 State가 변경될때 한번에 묶어서 한번만 리랜더 처리
-	- react17버전에서는 위와 같이 promise를 반환하는 동기 구문 콜백 안쪽에서 복수개의 state가 변경되면 여러번 리랜더가 일어남
+	useTransition 
+	- 연산에 우선순위를 둬서 좀 늦게 실행해도 될 것들을 지정
 */
